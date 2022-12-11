cab92f1a1ed45a933d8a60aa080bda740cb0d119
matt-shi-cs <fshi4@alumni.ncsu.edu>
2022-12-11 17:40:12 +0800

    [CHANGED]

--- features/gaussian_blur.cpp
+++ features/gaussian_blur.cpp
@@ -1,7 +1,7 @@
 /*** 
  * @Author: Matt.SHI
  * @Date: 2022-12-10 14:46:29
- * @LastEditTime: 2022-12-10 21:49:49
+ * @LastEditTime: 2022-12-11 17:08:21
  * @LastEditors: Matt.SHI
  * @Description: 
  * @FilePath: /opengl_demo/features/gaussian_blur.cpp
@@ -25,14 +25,21 @@
 
 #include <iostream>
 
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/video.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
 
 // settings
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
-int g_save_frame = 0;
+int g_save_frame = 1;
 int g_draw_frame = 1;
+cv::VideoCapture g_cam;
 unsigned char g_save_base_path[] = "./frames/";
 
 void saveFrameBuffer2PNG(const char* buf, 
@@ -47,8 +54,60 @@ void generateFilePath(char* filename, double index,const unsigned char* basePath
     sprintf(filename, "%s_%.4f.png", basePath,index);
 }
 
+void initCam()
+{
+    try
+    {
+        g_cam.open(0);
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    catch(...)
+    {
+        std::cerr << "error" << '\n';
+    }
+    
+}
+
+void readFrame(cv::Mat& frame)
+{
+    g_cam.read(frame);
+}
+
+unsigned int* create2DTexture(int count)
+{
+    unsigned int* pTextureIdxs = new unsigned int[count];
+    glGenTextures(count, pTextureIdxs);
+    for(int i = 0 ; i < count; i++)
+    {
+        glBindTexture(GL_TEXTURE_2D, pTextureIdxs[i]); // all upcoming GL_TEXTURE_2D operations now have effect on this texture object
+        // set the texture wrapping parameters
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT/*GL_REPEAT*/);	// set texture wrapping to GL_REPEAT (default wrapping method)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT/*GL_REPEAT*/);
+        // set boarder color
+        float borderColor[] = { 1.0f, 1.0f, 0.0f, 1.0f };
+        glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, borderColor);
+        // set texture filtering parameters
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR); 
+    }
+    return pTextureIdxs;
+}
+
+void updateTexture2DMemData(unsigned int textureIdx, int width, int height, int channel, 
+    unsigned int texturePixelFmt, unsigned int dataPixelFmt,unsigned char* data)
+{
+    glBindTexture(GL_TEXTURE_2D, textureIdx);
+    glTexImage2D(GL_TEXTURE_2D, 0, texturePixelFmt, width, height, 0, dataPixelFmt, GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+}
+
 int main(int argv,const char* argc[])
 {
+    //init cam
+    cv::Mat frameBufMat;
     // glfw: initialize and configure
     // ------------------------------
     glfwInit();
@@ -60,10 +119,14 @@ int main(int argv,const char* argc[])
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
     //processing input params
-    const char* input_sample_file = "resources/features_res/gaussain_bulr/test.png";
+    const char* input_data_path = nullptr;
     if(argv > 1)
     {
-        input_sample_file = argc[1];
+        input_data_path = argc[1];
+        std::cout << "[INPUT] use input file, path: " << input_data_path<<std::endl;
+    }
+    else{
+        std::cout << "[INPUT] use camera live stream: " << std::endl;
     }
 
     // glfw window creation
@@ -93,10 +156,10 @@ int main(int argv,const char* argc[])
 
     float vertices[] = {
         // positions          // colors           // texture coords
-         0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // top right
-         0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // bottom right
-        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // bottom left
-        -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // top left 
+         1.0f,  1.0f, 0.0f,   1.0f, 0.0f, 0.0f,   0.0f, 0.0f, // top right
+         1.0f, -1.0f, 0.0f,   0.0f, 1.0f, 0.0f,   0.0f, 1.0f, // bottom right
+        -1.0f, -1.0f, 0.0f,   0.0f, 0.0f, 1.0f,   1.0f, 1.0f, // bottom left
+        -1.0f,  1.0f, 0.0f,   1.0f, 1.0f, 0.0f,   1.0f, 0.0f  // top left 
     };
     unsigned int indices[] = {  
         0, 1, 3, // first triangle
@@ -128,42 +191,41 @@ int main(int argv,const char* argc[])
 
     // load and create a texture 
     // -------------------------
-    unsigned int texture;
-    glGenTextures(1, &texture);
-    glBindTexture(GL_TEXTURE_2D, texture); // all upcoming GL_TEXTURE_2D operations now have effect on this texture object
-    // set the texture wrapping parameters
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT/*GL_REPEAT*/);	// set texture wrapping to GL_REPEAT (default wrapping method)
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT/*GL_REPEAT*/);
-    // set boarder color
-    float borderColor[] = { 1.0f, 1.0f, 0.0f, 1.0f };
-    glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, borderColor);
-    // set texture filtering parameters
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    int texture2DCount = 1;
+    unsigned int* textureIdxs = create2DTexture(texture2DCount);
     
     // load image, create texture and generate mipmaps
     int width = 0, height = 0, nrChannels = 0,reqComp = 3;
-    stbi_set_flip_vertically_on_load(true); // tell stb_image.h to flip loaded texture's on the y-axis.
-    unsigned char *data = stbi_load(FileSystem::getPath(input_sample_file).c_str(), &width, &height, &nrChannels, reqComp);
-    if (nullptr != data)
+    if(nullptr != input_data_path)
     {
-        std::cout<<"width:"<<width<<" height:"<<height<<" nrChannels:"<<nrChannels<<std::endl;
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-        glGenerateMipmap(GL_TEXTURE_2D);
+        stbi_set_flip_vertically_on_load(true); // tell stb_image.h to flip loaded texture's on the y-axis.
+        unsigned char *data = stbi_load(input_data_path/*FileSystem::getPath(input_data_path).c_str()*/, &width, &height, &nrChannels, reqComp);
+        if (nullptr != data)
+        {
+            std::cout<<"[FILE]"<<input_data_path<<" width:"<<width<<" height:"<<height<<" nrChannels:"<<nrChannels<<std::endl;
+            updateTexture2DMemData(textureIdxs[0], width, height, nrChannels, GL_RGB,GL_RGB,data);
+            glViewport(0, 0, width, height);
+            stbi_image_free(data);
+        }
+        else
+        {
+            std::cout << "Failed to load texture" << std::endl;
+            return -1;
+        }
     }
-    else
-    {
-        std::cout << "Failed to load texture" << std::endl;
-        return -1;
+    else{
+        initCam();
+        readFrame(frameBufMat);
+        width = frameBufMat.cols;
+        height = frameBufMat.rows;
+        std::cout << "[CAMERA] inited:  width = "<<width << " height = " << height << std::endl;
     }
-    stbi_image_free(data);
     //texture end
 
     //using framebuffer
     FrameBuffer fbo;
     fbo.init(width, height);        // for single-sample FBO
-    //============================
-            
+    //================================================================================================     
     // all upcoming GL_TEXTURE_2D operations now have effect on this texture object
 
     double startTime = 0.0;
@@ -175,14 +237,27 @@ int main(int argv,const char* argc[])
     while (!glfwWindowShouldClose(window))
     {
         processInput(window);
+        //read cam
+        if(!frameBufMat.empty())
+        {
+            readFrame(frameBufMat);
+        }
+
+        startTime = glfwGetTime();
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        // bind Texture
+        glBindTexture(GL_TEXTURE_2D, textureIdxs[0]);
+
+        if(!frameBufMat.empty())
+        {
+            updateTexture2DMemData(textureIdxs[0], frameBufMat.cols, frameBufMat.rows, 3,
+                GL_RGB,GL_BGR, frameBufMat.data);
+        }
 
         // render
         //fbo.bind();
 
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-        // bind Texture
-        glBindTexture(GL_TEXTURE_2D, texture);
         // render container
         ourShader.use();
         glBindVertexArray(VAO);
@@ -190,24 +265,20 @@ int main(int argv,const char* argc[])
         //fbo.update();
         //fbo.unbind();
 
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-
-        GLuint texId = fbo.getColorId();        // texture object ID for render-to-texture
-        const unsigned char* buffer = fbo.getColorBuffer();
+        //glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        endTime = glfwGetTime();
 
         frameIndex++;
-        endTime = glfwGetTime();
-        if(endTime - startTime > 1.0)
+        if(endTime - startTime >= 0.001)
         {
             std::cout << "[Frame]The fps is " <<  frameIndex / (endTime - startTime) << std::endl;
             if(g_save_frame > 0)
             {
-                g_save_frame = 0;
                 char save_path[256] = {0};
                 generateFilePath(save_path,endTime, g_save_base_path);
+                const unsigned char* buffer = fbo.getColorBuffer();
                 saveFrameBuffer2PNG((const char*)(buffer), width, height, 4,save_path);
             }
-            startTime = endTime; 
             frameIndex = 0;
         }
         
